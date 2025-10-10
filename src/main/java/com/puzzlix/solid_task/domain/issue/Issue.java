b060c6d68652da18c2f5068863ea48df021cdd82
seongyob99 <seongyob99@naver.com>
2025-10-10 10:42:02 +0900

    유저, 이슈, 프로젝트 도메인 객체 설계

---
+++ src/main/java/com/puzzlix/solid_task/domain/issue/Issue.java
@@ -0,0 +1,27 @@
+package com.puzzlix.solid_task.domain.issue;
+
+public class Issue {
+    // pk
+    // 타이틀
+    // 내용
+    // 진행 상태
+
+    // 프로젝트 pk
+    // 보고자 (누가 요청 했는지)
+    // 담당자
+
+    private Long id;
+    private String title;
+    private String description;
+    private IssueStatus issueStatus;
+
+    // 추후 연관관계 필드
+    private Long projectId;
+
+    // 누가 요청(보고)
+    private Long reporterId;
+
+    // 담당자
+    private Long assigneeId;
+
+}
